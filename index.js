9af223501f67eed4d4376b6ed0453970aaa5a1e9
Barnaby Fredric <36432141+quickreactor@users.noreply.github.com>
2024-12-24 23:03:16 +1300

    update port

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.delete("/api/persons/:id", (request, response) => {
     persons = persons.filter((n) => n.id !== id);
     response.sendStatus(204);
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
